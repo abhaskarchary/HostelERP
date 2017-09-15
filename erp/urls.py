0d175fc1568b8a510ee24b98a001b5f984245e29
IshanRD19 <ishanlsr@gmail.com>
2017-09-15 14:37:19 +0530

    stable ishan

--- erp/urls.py
+++ erp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'^student/', include('student.urls')),
     url(r'^manager/', include('manager.urls')),
     url(r'^dues/', include('payfees.urls')),
+    url(r'^view_rooms/', include('Room.urls')),
     url(r'^$', views.home),
 
 ]
